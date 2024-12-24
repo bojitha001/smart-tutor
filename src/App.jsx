351b89e4d15e8fd2cecbbad8d527d449f44e776b
bojithanawarathna <nawarathnabojitha@gmail.com>
2024-12-24 22:58:38 +0530

    init-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.css'
 import { SignUpAuth } from "./components/SignUpAuth";
 import { Navbar } from "./components/NavBar";
 import { HomePage } from "./components/HomePage";
--- src/components/SignUpAuth.jsx
+++ src/components/SignUpAuth.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { auth, googleProvider } from "../config/firebase";
+import "../.ExternalCss/SignUp.css";
 import {createUserWithEmailAndPassword, signInWithPopup} from 'firebase/auth';
 
 export const SignUpAuth = () => {
@@ -33,117 +34,138 @@ export const SignUpAuth = () => {
     }
     
     return(
-        <>
-        <div className="row g-3 m-2 p-4">
-            <div className="col-md-5 p-5 bg-primary text-white rounded">
-                <div>
-                    <h3>Empower Your Learning </h3>
-                    <h3>Journey - Join Us Today!</h3>
-                </div><br/>
-                <div>Join us to unlock new skills and advance with hands-on learning and expert guidance. Start transforming your abilities today!</div>
-            </div>
-            <div className="col-md-1"></div>
-            <div className="col-md-5">
-                <form className="row g-3" onSubmit={signUp}>
-                <h3 className="text-center">Let's get started !</h3>
-
-                <div className="col-md-6">
-                    <label className="form-label">First Name</label>
-                    <input
-                    type="text"
-                    className="form-control"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    required
-                    />
-                </div>
-                <div className="col-md-6">
-                    <label className="form-label">Last Name</label>
-                    <input
-                    type="text"
-                    className="form-control"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    required
-                    />
-                </div>
-                <div className="col-md-6">
-                    <label className="form-label">Date of Birth</label>
-                    <input
-                    className="form-control"
-                    type="date"
-                    value={dateOfBirth}
-                    onChange={(e) => setDateOfBirth(e.target.value)}
-                    required
-                    />
-                </div>
-                <div className="col-md-6">
-                    <label className="form-label">Phone Number</label>
-                    <input
-                    className="form-control"
-                    type="tel"
-                    value={phoneNumber}
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                    required
-                    />
-                </div>
-                <div className="col-md-12">
-                    <label className="form-label">Email</label>
-                    <input
-                    className="form-control"
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    />
-                </div>
-                <div className="col-md-6">
-                    <label className="form-label">Password</label>
-                    <input
-                    type="password"
-                    className="form-control"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    />
-                </div>
-                <div className="col-md-6">
-                    <label className="form-label">Confirm Password</label>
-                    <input
-                    type="password"
-                    className="form-control"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    required
-                    />
-                </div>
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked/>
-                    <label className="form-check-label" htmlFor="flexCheckChecked">
-                    I agree to the Terms and Privacy Policy and accept responsibility for 
-                    my account's security
-                    </label>
-                </div>
-                <div className="col-12 text-center">
-                    <button type="submit" className="btn btn-primary w-100">
-                        Create Account
-                    </button>
-                </div>
-
-                <div className="col-12 text-center">
-                    <p>OR</p>
-                    <button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={signUpWithGoogle}
-                    >
-                        Sign Up with Google
-                    </button>
-                </div>
-                <div className="text-center">Already have an account ?&nbsp;&nbsp;<span className="text-primary"><a href="#" className="text-decoration-none">Login</a></span></div>
-                </form>
-            </div>
+        
+        <div className="row g-3 p-2 container-main">
+        <div className="col-md-5 p-5 bg-primary text-white rounded">
+          <div className="main-txt">
+          <div class="logo">
+        <p>
+          SMART<span class="smart">TUTOR</span>
+        </p>
+      </div>
+            <h3 className="main-title">Empower Your Learning</h3>
+            <h3 className="main-title">Journey - Join Us Today!</h3>
+          </div>
+          <br />
+          <p className="main-description">
+          Join us to unlock new skills and advance with hands-on learning and expert guidance. Start transforming your abilities today!
+          </p>
         </div>
-        </>
+        <div className="col-md-1"></div>
+        <div className="col-md-5 container-left">
+          <form className="row g-3" onSubmit={signUp}>
+            <h3 className="text-center">Let's get started!</h3>
+  
+            <div className="col-md-6">
+              <label className="form-label">First Name</label>
+              <input
+                type="text"
+                className="form-control"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Last Name</label>
+              <input
+                type="text"
+                className="form-control"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Date of Birth</label>
+              <input
+                className="form-control"
+                type="date"
+                value={dateOfBirth}
+                onChange={(e) => setDateOfBirth(e.target.value)}
+                required
+              />
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Phone Number</label>
+              <input
+                className="form-control"
+                type="tel"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                required
+              />
+            </div>
+            <div className="col-md-12">
+              <label className="form-label">Email</label>
+              <input
+                className="form-control"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Confirm Password</label>
+              <input
+                type="password"
+                className="form-control"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckChecked"
+                defaultChecked
+              />
+              <label className="form-check-label" htmlFor="flexCheckChecked">
+                I agree to the Terms and Privacy Policy and accept responsibility
+                for my account's security
+              </label>
+            </div>
+            <div className="col-12 text-center">
+              <button type="submit" className="btn btn-primary w-100">
+                Create Account
+              </button>
+            </div>
+  
+            <div className="col-12 text-center">
+              <p>OR</p>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={signUpWithGoogle}
+              >
+                Sign Up with Google
+              </button>
+            </div>
+            <div className="text-center">
+              Already have an account?&nbsp;&nbsp;
+              <span className="text-primary">
+                <a href="#" className="text-decoration-none">
+                  Login
+                </a>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+      
     );
 }
