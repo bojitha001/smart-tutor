f7edeaadc777a00c6341b86279a8027b42d34ab2
bojitha001 <nawarathnabojitha@gmail.com>
2025-01-24 21:38:39 +0530

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import "./App.css";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.css'
-import { SignUpAuth } from "./components/SignUpAuth";
+import "bootstrap/dist/css/bootstrap.css";
+// import { SignUpAuth } from "./components/SignUpAuth";
 import { Navbar } from "./components/NavBar";
 import { HomePage } from "./components/HomePage";
-import { SignUpOptions} from  "./components/tutorSignUp/SignUpOptions"
+import { SignUpOptions } from "./components/tutorSignUp/SignUpOptions";
 import { SignUpQuestions } from "./components/tutorSignUp/SignUpQuestions";
+import Test from "./components/Test";
 
-
-function App(){
-  return(
+function App() {
+  return (
     <>
       <div className="App">
-        {/* <Navbar/> */}
-        {/* <HomePage/> */}
-        <SignUpQuestions/>
-        <SignUpOptions/>
+        <Navbar />
+        <HomePage />
+        <Test />
+        {/* <SignUpQuestions/> */}
+        {/* <SignUpOptions/> */}
       </div>
     </>
   );
