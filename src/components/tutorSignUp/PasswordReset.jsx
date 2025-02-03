a07e5ea1864853bc89df85ff85e2f400a1a515b5
Adithya306 <garukaadithya@gmail.com>
2025-02-03 19:34:08 +0530

    delete normal css file import

--- src/components/tutorSignUp/PasswordReset.jsx
+++ src/components/tutorSignUp/PasswordReset.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 import { getFirestore, collection, query, where, getDocs } from "firebase/firestore"; //Import Firestore functions
-import '../../.ExternalCss/PasswordReset.css';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
