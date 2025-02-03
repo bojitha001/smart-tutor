2698c148382e8623029e066b70fc95aafe2333fd
bojithanawarathna <nawarathnabojitha@gmail.com>
2025-02-03 12:03:47 +0530

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "./index.css";
+// import "./.ExternalCss/Kuppi.css"
 import 'bootstrap/dist/css/bootstrap.css'
-import { SignUpAuth } from "./components/SignUpAuth";
+// import { SignUpAuth } from "./components/SignUpAuth";
 import { Navbar } from "./components/NavBar";
 import { HomePage } from "./components/HomePage";
 import { SignUpOptions} from  "./components/tutorSignUp/SignUpOptions"
 import { SignUpQuestions } from "./components/tutorSignUp/SignUpQuestions";
+
 
 
 function App(){
@@ -13,9 +15,10 @@ function App(){
     <>
       <div className="App">
         {/* <Navbar/> */}
-        {/* <HomePage/> */}
-        <SignUpQuestions/>
-        <SignUpOptions/>
+        
+        {/* <HomePage/>  */}
+        {/* <SignUpQuestions/> */}
+        {/* <SignUpOptions/> */}
       </div>
     </>
   );
