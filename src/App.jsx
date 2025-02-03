79443c38a159899b715011250e6afda3af93faa9
Adithya306 <garukaadithya@gmail.com>
2025-02-03 15:50:10 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import "./App.css";
 // import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 // import { SignUpAuth } from "./components/SignUpAuth";
 import styles from "../src/.ExternalCss/Sidebar.module.css";
 import { Navbar } from "./components/NavBar";
@@ -11,10 +11,9 @@ import { SignUpOptions } from "./components/tutorSignUp/SignUpOptions";
 import { SignUpQuestions } from "./components/tutorSignUp/SignUpQuestions";
 import BecomeATutor from "./components/BecomeATutor";
 import { ResetPassword } from "./components/tutorSignUp/PasswordReset";
-
-
 import { BrowserRouter as Router } from 'react-router-dom';
 import Sidebar from "./components/tutorDashboard/Sidebar";
+import MainContent from "./components/tutorDashboard/MainContent";
 // import SelectingSubjects from "./components/Parent/Student/SelectingSubjects";
 
 
@@ -22,7 +21,7 @@ function App() {
   return (
     <>
     
-      {/* <div className="App"> */}
+      <div className="App"> 
           {/* <Navbar />
          <BecomeATutor />    */}
         {/* <Sidebar/> */}
@@ -39,15 +38,15 @@ function App() {
         
         
 
-      {/* </div> */}
+      </div>
 
 
-      <Router>
+      {/* <Router>
       <div className={styles.app}>
         <Sidebar />
-        <MainContent />
+        <MainContent/>
       </div>
-    </Router>
+    </Router> */}
       
       
     </>
