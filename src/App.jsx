e65d9e2c9437fc08d1ffdd44e929b65557bfd90c
Vihan Mendis <62713216+Vihan12345@users.noreply.github.com>
2025-02-06 15:46:20 +0530

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,18 @@ import { SignUpOptions } from "./components/tutorSignUp/SignUpOptions";
 import { SignUpQuestions } from "./components/tutorSignUp/SignUpQuestions";
 import BecomeATutor from "./components/BecomeATutor";
 
+
 function App() {
   return (
     <>
       <div className="App">
         <Navbar />
-        <BecomeATutor />
-        {/* <HomePage /> */}
+        {/* <BecomeATutor /> */}
+        <HomePage />
 
         {/* <SignUpQuestions/> */}
         {/* <SignUpOptions/> */}
+        
       </div>
     </>
   );
