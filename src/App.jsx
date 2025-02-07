4962fed5876abbfa95b298e33a1bc1f9db02d06a
bojithanawarathna <nawarathnabojitha@gmail.com>
2025-02-07 11:40:00 +0530

    start implementing the student signup

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,11 @@ import DashboardClasses from "./components/Dashbaord/DashboardClasses";
 // import DashBoard from "./components/Dashbaord/DashboardClasses"
 
 
-// import MainContent from "./components/tutorDashboard/MainContent";
+import MainContent from "./components/tutorDashboard/MainContent";
 
 // import MainContent from "../components/tutorDashboard/MainContent";
-// import StudentView from "../components/tutorDashboard/StudentSection";
+import StudentView from "./components/tutorDashboard/StudentSection";
+import StudentSignUp from "./components/StudentSignUp/StudentSignUp";
 
 
 // import SelectingSubjects from "./components/Parent/Student/SelectingSubjects";
@@ -33,47 +34,25 @@ function App() {
   return (
 
       <div className="App"> 
-          {/* <Navbar />
-         <BecomeATutor />    */}
+           {/* <Navbar /> */}
+         {/* <BecomeATutor />     */}
         {/* <Sidebar/> */}
         {/* <HomePage /> */}
         {/* <SelectingSubjects /> */}
         {/* <SignUpQuestions/> */}
-        {/* <SignUpQuestions/> */}
         {/* <SignUpOptions/> */}
         {/* <SignInAuth/> */}
         {/* <ResetPassword/> */}
-
+        <StudentSignUp />
         
-       <DashboardClasses />
+       {/* <DashboardClasses /> */}
 
-        
-
-          {/* <Sidebar/> */}
-        
-        
-
-      
-
-
-
-
-
-
-      {/* <Router>
-      <div className={styles.app}>
-        <Sidebar />
-        <MainContent/>
-        <StudentView/>
-      </div>
-    </Router> */}
-      
-
+          
       {/* <Router>
       <div className={styles.app}>
         <Sidebar/>
         <Routes>
-        <    Route path="/" element={<MainContent />} />
+        <Route path="/" element={<MainContent />} />
             
             <Route path="/students" element={<StudentView />} />
         </Routes>
