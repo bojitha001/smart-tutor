c726f42af80b9cd6386a439aa03e50899fad9909
Bojitha Nawarathna <nawarathnabojitha@gmail.com>
2025-03-05 15:19:26 +0530

    init commit

--- src/components/BecomeATutor.jsx
+++ src/components/BecomeATutor.jsx
@@ -7,6 +7,7 @@ import studentMonitoring from "../assets/images/Student Monitoring.png";
 import instantPayment from "../assets/images/Instant Payment.png";
 import avatar1 from "../assets/images/Avatar1.jpg";
 import avatar2 from "../assets/images/Avatar2.jpg";
+import Footer from "./Footer";
 
 const BecomeATutor = () => {
   return (
@@ -192,111 +193,7 @@ const BecomeATutor = () => {
         </div>
       </div>
 
-      <footer className={`${styles.footer}`}>
-      <div className={`${styles["footer-grid"]} ${styles.container}`}>
-        <nav className={`${styles["nav-col"]}`}>
-          <p className={`${styles["footer-heading"]}`}>Site Map</p>
-          <ul className={`${styles["footer-nav"]}`}>
-            <li>
-              <a className={`${styles["footer-link"]}`} href="#">
-                Home
-              </a>
-            </li>
-            <li>
-              <a className={`${styles["footer-link"]}`} href="#">
-                About us
-              </a>
-            </li>
-            <li>
-              <a className={`${styles["footer-link"]}`} href="#">
-                Why Us
-              </a>
-            </li>
-            <li>
-              <a className={`${styles["footer-link"]}`} href="#">
-                Blog
-              </a>
-            </li>
-          </ul>
-        </nav>
-
-        <nav className={`${styles["nav-col"]}`}>
-          <p className={`${styles["footer-heading"]}`}>Legal</p>
-          <ul className={`${styles["footer-nav"]}`}>
-            <li>
-              <a className={`${styles["footer-link"]}`} href="#">
-                General Info
-              </a>
-            </li>
-            <li>
-              <a className={`${styles["footer-link"]}`} href="#">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a className={`${styles["footer-link"]}`} href="#">
-                Terms and Conditions
-              </a>
-            </li>
-          </ul>
-        </nav>
-
-        <nav className={`${styles["nav-col"]}`}>
-          <p className={`${styles["footer-heading"]}`}>Services</p>
-          <ul className={`${styles["footer-nav"]}`}>
-            <li>
-              <a className={`${styles["footer-link"]}`} href="#">
-                Home
-              </a>
-            </li>
-            <li>
-              <a className={`${styles["footer-link"]}`} href="#">
-                About us
-              </a>
-            </li>
-            <li>
-              <a className={`${styles["footer-link"]}`} href="#">
-                Pricing Plan
-              </a>
-            </li>
-            <li>
-              <a className={`${styles["footer-link"]}`} href="#">
-                Blog
-              </a>
-            </li>
-          </ul>
-        </nav>
-
-        <nav className={`${styles["nav-col"]}`}>
-          <p className={`${styles["footer-heading"]}`}>Contact Us</p>
-          <ul className={`${styles["footer-nav"]}`}>
-            <li className={`${styles["footer-nav-icon"]}`}>
-              <Phone />
-              <a className={`${styles["footer-link"]}`} href="">
-                +94 76 869 4195
-              </a>
-            </li>
-            <li className={`${styles["footer-nav-icon"]}`}>
-              <Mail />
-              <a className={`${styles["footer-link"]}`} href="">
-                smarttutor0019@gamil.com
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div className={`${styles["footer-bottom"]}`}>
-        <hr />
-        <div className={`${styles["footer-bottom-logo-sec"]}`}>
-          <p>SMART <span>TUTOR</span></p>
-          <ul className={`${styles.logos}`}>
-            <li><a className={`${styles["footer-logo"]}`} href="#"><Facebook /></a></li>
-            <li><a className={`${styles["footer-logo"]}`} href="#"></a><Instagram /></li>
-            <li><a className={`${styles["footer-logo"]}`} href="#"></a><Linkedin /></li>
-          </ul>
-        </div>
-      </div>
-    </footer>
+      <Footer/>
     </>
   );
 };
