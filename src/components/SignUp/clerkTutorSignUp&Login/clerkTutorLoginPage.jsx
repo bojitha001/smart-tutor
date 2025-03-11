cd425738022cc8a2b3b805dd20211901748590eb
lashan <ravijalashangunawardena@gmail.com>
2025-03-12 01:13:30 +0530

    login and resetpassword

--- src/components/SignUp/clerkTutorSignUp&Login/clerkTutorLoginPage.jsx
+++ src/components/SignUp/clerkTutorSignUp&Login/clerkTutorLoginPage.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { SignIn } from "@clerk/clerk-react";
+import styles from "../../../.ExternalCss/clerkLogin.module.css"
 
 export const ClerkLogin = () => {
 
   return (
     <>
-      <div className="flex min-h screen justify-center items-center">
+      <div className={`${styles['sign-in-container']}`}>
         <SignIn />
       </div>;
     </>
