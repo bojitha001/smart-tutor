36a78e3ca4b0c5622b7adad40d15dd61edd92381
Bojitha Nawarathna <nawarathnabojitha@gmail.com>
2025-03-11 20:46:20 +0530

    Fetch the data that gets by submitting the question form of kuppi groups

--- src/components/KuppiGroups/QuestionForm.jsx
+++ src/components/KuppiGroups/QuestionForm.jsx
@@ -12,8 +12,30 @@ const getCommunityById = async (id) => {
   // console.log(jobs);
 };
 
+const createCommunityQuestionForm = async (questionForm) => {
+  const res = await fetch("http://localhost:8080/comunityQuestions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", //saying we are passing a json
+    },
+    body: JSON.stringify(questionForm),
+  });
+};
+
+export const getCommunityQuestionForm = async (id) => {
+  const res = await fetch(
+    `http://localhost:8080/comunityQuestions?communityId=${id}`,
+    {
+      method: "GET",
+    }
+  );
+  const data = await res.json();
+  return data;
+};
+
 const QuestionForm = () => {
   const [communities, setCommunities] = useState(null);
+  const [questionForm, setQuestionForm] = useState([]);
   const params = useParams();
 
   useEffect(() => {
@@ -23,26 +45,48 @@ const QuestionForm = () => {
       })
       .catch((err) => {})
       .finally(() => {});
-  }, [params]);
+
+    getCommunityQuestionForm(params.id)
+      .then((data) => {
+        setQuestionForm(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [params, setQuestionForm]);
 
   const [questions, setQuestions] = useState([
     {
-      questions: "The component below uses Material UI Datagrid Premium to display a list of scholarships, and if students have applied for the scholarship, they are grouped by scholarshipName and ex",
+      questions:
+        "The component below uses Material UI Datagrid Premium to display a list of scholarships, and if students have applied for the scholarship, they are grouped by scholarshipName and ex",
       subQ: "The component below uses Material UI Datagrid Premium to display a list of scholarships, and if students have applied for the scholarship, they are grouped by scholarshipName and ex",
     },
   ]);
+
+  const [formData, setFormData] = useState({
+    topic: "",
+    questions: " ",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    createCommunityQuestionForm({
+      userId: "123",
+      topic: formData.topic,
+      questions: formData.questions,
+      community: params.id,
+    });
+  };
 
   return (
     <>
       <div className={`${styles["questions-container"]}`}>
         <div className={`${styles["questions-box"]}`}>
-          <div>
-            
-          </div>
-          {questions.map((question) => (
-            <div>
+          {questionForm.map((question) => (
+            <div key={question.id}>
+              <p>{question.topic}</p>
               <p>{question.questions}</p>
-              <p>{question.subQ}</p>
             </div>
           ))}
         </div>
@@ -50,29 +94,30 @@ const QuestionForm = () => {
       <div>
         <h1>{`Welcome to ${communities?.name}`}</h1>
       </div>
-      <div className={styles.questionInput}>
-        <input
-          type="text"
-          placeholder="Enter your topic here"
-          className={styles.topicInput}
-          // value={newTopic}
-          // onChange={(e) => setNewTopic(e.target.value)}
-        />
-        <textarea
-          placeholder="Describe your question here"
-          className={styles.questionTextarea}
-          // value={newQuestion}
-          // onChange={(e) => setNewQuestion(e.target.value)}
-        ></textarea>
-
-        <button
-          className={styles.postQuestionButton}
-          // onClick={handlePostQuestion}
-          // disabled={!newTopic  !newQuestion  !selectedCommunityForPost}
-        >
-          Post Question
-        </button>
-      </div>
+      <form action="" onSubmit={handleSubmit}>
+        <div className={styles.questionInput}>
+          <input
+            type="text"
+            placeholder="Enter your topic here"
+            className={styles.topicInput}
+            required
+            value={formData.topic}
+            onChange={(event) =>
+              setFormData({ ...formData, topic: event.target.value })
+            }
+          />
+          <textarea
+            placeholder="Describe your question here"
+            className={styles.questionTextarea}
+            required
+            value={formData.questions}
+            onChange={(event) =>
+              setFormData({ ...formData, questions: event.target.value })
+            }
+          ></textarea>
+          <button className={styles.postQuestionButton}>Post Question</button>
+        </div>
+      </form>
     </>
   );
 };
