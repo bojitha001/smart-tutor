c564fa8610ffe65b7833799ad87fe539765eb57a
lashan <ravijalashangunawardena@gmail.com>
2025-03-11 22:56:06 +0530

    clerk continued

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,26 +1,26 @@
-import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; 
+// import { initializeApp } from "firebase/app";
+// // import { getAnalytics } from "firebase/analytics";
+// import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore"; 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBHwbdhO52Vt6aQ7CflhRoh13PorW7Sfa0",
-    authDomain: "smart-tutor-5dbe1.firebaseapp.com",
-    projectId: "smart-tutor-5dbe1",
-    storageBucket: "smart-tutor-5dbe1.firebasestorage.app",
-    messagingSenderId: "161513714945",
-    appId: "1:161513714945:web:bd9281357ff70b2eb31700",
-    measurementId: "G-11WGJKEBM3"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBHwbdhO52Vt6aQ7CflhRoh13PorW7Sfa0",
+//     authDomain: "smart-tutor-5dbe1.firebaseapp.com",
+//     projectId: "smart-tutor-5dbe1",
+//     storageBucket: "smart-tutor-5dbe1.firebasestorage.app",
+//     messagingSenderId: "161513714945",
+//     appId: "1:161513714945:web:bd9281357ff70b2eb31700",
+//     measurementId: "G-11WGJKEBM3"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-const db = getFirestore(app); 
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
+// const auth = getAuth(app);
+// const googleProvider = new GoogleAuthProvider();
+// const db = getFirestore(app); 
 
-export {auth, googleProvider, db}
+// export {auth, googleProvider, db}
 
 
 
