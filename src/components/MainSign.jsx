086065542456513e6d7785abc29874a4e376b053
Bojitha Nawarathna <nawarathnabojitha@gmail.com>
2025-03-12 17:36:30 +0530

    init commit

--- src/components/MainSign.jsx
+++ src/components/MainSign.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import styles from '../.ExternalCss/mainSign.module.css'
-import img from '../assets/images/ChooseSign.png'
+import React from "react";
+import styles from "../.ExternalCss/mainSign.module.css";
+import img from "../assets/images/ChooseSign.png";
 
 const MainSign = () => {
-    return (
-        <div className={`${styles["main-container"]}`}>
-            <div className={`${styles["main-container-left"]}`}>
-                <img className={`${styles["main-img"]}`} src={img} alt="" />
-            </div>
-            <div className={`${styles["main-container-right"]}`}>
-                <div className={`${styles["choose-sign-up"]}`}>
-                    <p>Tutor</p>
-                </div>
-                <div className={`${styles["choose-sign-up"]}`}>
-                    <p>Tutor</p>
-                </div>
-                <div className={`${styles["choose-sign-up"]}`}>
-                    <p>Tutor</p>
-                </div>
-            </div>
+  return (
+    <div className={`${styles["main-container"]}`}>
+      <div className={`${styles["main-container-left"]}`}>
+        <img className={`${styles["main-img"]}`} src={img} alt="" />
+      </div>
+      <div className={`${styles["main-container-right"]}`}>
+        <div className={`${styles["choose-sign-up"]}`}>
+          <div>
+            <button>Tutor</button>
+          </div>
         </div>
-    )
-}
+        <div className={`${styles["choose-sign-up"]}`}>
+          <div>
+            <button>Student</button>
+          </div>
+        </div>
+        <div className={`${styles["choose-sign-up"]}`}>
+          <div>
+            <button>Parent</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MainSign
+export default MainSign;
